c81fc3d83f102559ae530945f81712dec34a1c86
unknown <kodywinq1@gmail.com>
2022-10-15 10:00:49 +0300

    add: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,19 +8,69 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.rootTree = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootTree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+    if(!this.rootTree){
+      this.rootTree = new Node(value);
+      return;
+    }
+
+    let currentNode = this.rootTree;
+
+    while(true){
+      console.debug(currentNode);
+        if(value > currentNode.data){
+          if(!currentNode.right){
+            currentNode.right = new Node(value);
+            break;
+          }
+          else
+            currentNode = currentNode.right;
+        }
+        if(value < currentNode.data) {
+          if (!currentNode.left){
+            currentNode.left = new Node(value);
+            break;
+          }
+          else
+            currentNode = currentNode.right;
+        }
+      }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if(!this.rootTree){
+      return false;
+    }
+    let currentNode = this.rootTree;
+    console.debug(currentNode);
+    while(true){
+      if(data > currentNode.data){
+        if(!currentNode.right){
+          console.debug("нет");
+          return false;
+        }
+        else
+          currentNode = currentNode.right;
+      }
+      else if(data < currentNode.data) {
+        if (!currentNode.left){
+          console.debug("нет");
+          return false;
+        }
+        else
+          currentNode = currentNode.right;
+      }
+      else
+        return true;
+    }
   }
 
   find(/* data */) {
